fc2e86cc4ac0a353c2df4fa9624660756e6631c7
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-09-29 09:56:36 +0330

    migrate project to java-library plugin and restructure dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   kotlin("jvm") version "1.9.25"
   kotlin("plugin.spring") version "1.9.25"
-  id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
+  `java-library`
 }
 
 group = "io.github.mahdibohloul"
@@ -26,11 +26,20 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
-  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+  implementation("org.springframework:spring-context:6.2.10")
+  implementation("org.springframework.boot:spring-boot-autoconfigure:3.5.5")
+  implementation("io.projectreactor:reactor-core:3.7.9")
+  implementation("io.projectreactor.kafka:reactor-kafka:1.3.24")
+  implementation("box.tapsi.libs:utilities-starter:0.9.2")
+  implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.4")
+  implementation("io.projectreactor.addons:reactor-extra:3.5.3")
+  implementation("org.slf4j:slf4j-api:2.0.17")
+  implementation("com.google.protobuf:protobuf-java:3.25.5")
+
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.5")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.23")
+  testImplementation("io.projectreactor:reactor-test:3.7.9")
+
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ src/main/kotlin/io/github/mahdibohloul/spring/reactor/kafka/autoconfigure/ReactiveKafkaAutoConfiguration.kt
@@ -0,0 +1,7 @@
+package io.github.mahdibohloul.spring.reactor.kafka.autoconfigure
+
+import org.springframework.boot.autoconfigure.AutoConfiguration
+
+@AutoConfiguration
+class ReactiveKafkaAutoConfiguration {
+}
