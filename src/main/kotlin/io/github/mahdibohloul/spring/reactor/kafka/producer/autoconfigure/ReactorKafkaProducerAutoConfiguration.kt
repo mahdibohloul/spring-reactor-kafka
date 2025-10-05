436081961a4178b27576b559d269beae7e8482b5
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-10-05 15:38:45 +0330

    add `ReactorKafkaProducerAutoConfiguration` for auto-configuring Kafka producer components and update Spring AutoConfiguration imports

---
+++ src/main/kotlin/io/github/mahdibohloul/spring/reactor/kafka/producer/autoconfigure/ReactorKafkaProducerAutoConfiguration.kt
@@ -0,0 +1,30 @@
+package io.github.mahdibohloul.spring.reactor.kafka.producer.autoconfigure
+
+import io.github.mahdibohloul.spring.reactor.kafka.autoconfigure.ReactiveKafkaAutoConfiguration
+import io.github.mahdibohloul.spring.reactor.kafka.producer.ProducerProperties
+import io.github.mahdibohloul.spring.reactor.kafka.producer.annotations.OnKafkaProducerEnabled
+import io.github.mahdibohloul.spring.reactor.kafka.producer.pubsub.publishers.KafkaPublisher
+import io.github.mahdibohloul.spring.reactor.kafka.producer.services.KafkaProducerService
+import io.github.mahdibohloul.spring.reactor.kafka.producer.services.KafkaProducerServiceImpl
+import io.github.mahdibohloul.spring.reactor.kafka.producer.services.KafkaProducerServiceMockImpl
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+
+@EnableConfigurationProperties(ProducerProperties::class)
+@AutoConfiguration(after = [ReactiveKafkaAutoConfiguration::class])
+@Import(ProducerProperties::class, KafkaPublisher::class)
+class ReactorKafkaProducerAutoConfiguration {
+  @Bean
+  @OnKafkaProducerEnabled
+  fun kafkaProducerService(applicationContext: ApplicationContext): KafkaProducerService =
+    KafkaProducerServiceImpl(applicationContext)
+
+  @Bean
+  @ConditionalOnMissingBean(KafkaProducerServiceImpl::class)
+  fun mockKafkaProducerService(): KafkaProducerService =
+    KafkaProducerServiceMockImpl()
+}
